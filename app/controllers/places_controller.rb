8be674199e7934f0b73434e241184ad040e753d1
Mason Bartlett <Mbartlett413@me.com>
2017-03-02 00:04:54 +0000

    edit/delete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,6 +22,19 @@ class PlacesController < ApplicationController
   def edit
     @place = Place.find(params[:id])
   end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
+  end
+
 #Yo, don't go down there, its private...
   private
 
