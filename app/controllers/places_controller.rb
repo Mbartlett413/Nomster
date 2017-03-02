35bed6588478581c9b4c290b98230470fae88776
Mason Bartlett <Mbartlett413@me.com>
2017-03-02 17:46:10 +0000

    Validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,11 +5,11 @@ class PlacesController < ApplicationController
     @places = Place.all.paginate(:page => params[:page], :per_page => 3)
   end
 
-
+# NEW
   def new 
     @place = Place.new
   end
-
+# CREATE
   def create
       @place = current_user.places.create(place_params)
       if @place.valid?
@@ -18,31 +18,33 @@ class PlacesController < ApplicationController
         render :new, status: :unprocessable_entity
       end 
   end
-
+# SHOW
   def show
     @place = Place.find(params[:id])
   end
-
+# EDIT 
   def edit
     @place = Place.find(params[:id])
       if @place.user != current_user
         return render text: 'Not Allowed', status: :forbidden
       end
   end
-
+# UP DATE
   def update
     @place = Place.find(params[:id])
+      
       if @place.user != current_user
         return render text: 'Not Allowed', status: :forbidden
       end
+
     @place.update_attributes(place_params)
-      if @place.valid
+      if @place.valid?
       redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
     end
   end
-
+# DESTROY 
   def destroy
     @place = Place.find(params[:id])
     if @place.user != current_user
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, length: { minimum: 2, too_long: "%{count} Character is the maximum allowed" }, presence: true
+  validates :address, presence: true
+  validates :description, length: { maximum: 200, too_long: "%{count} Character is the maximum allowed" }, presence: true
 end
